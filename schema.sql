beef306c400f23c7e10676ec9209b652990c9478
Mariana Barnett <marianabarnett3@gmail.com>
2020-05-25 10:58:37 -0400

    new commit

--- schema.sql
+++ schema.sql
@@ -1,7 +1,14 @@
+
 -- * **department**:
 
 --   * **id** - INT PRIMARY KEY
 --   * **name** - VARCHAR(30) to hold department name
+
+CREATE TABLE department (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
 
 -- * **role**:
 
@@ -10,6 +17,15 @@
 --   * **salary** -  DECIMAL to hold role salary
 --   * **department_id** -  INT to hold reference to department role belongs to
 
+ CREATE TABLE role (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(30) NOT NULL, 
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+
 -- * **employee**:
 
 --   * **id** - INT PRIMARY KEY
@@ -17,3 +33,13 @@
 --   * **last_name** - VARCHAR(30) to hold employee last name
 --   * **role_id** - INT to hold reference to role employee has
 --   * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
+
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
